7af7a9af94a78b4575e8e6d303b9a7ab82a96540
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-12-31 11:38:20 +0700

    feat: adds minitask 4

---
+++ query1.sql
@@ -0,0 +1,14 @@
+-- Point 1
+SELECT *
+FROM movies
+WHERE extract(year FROM release_date) = 2020;
+
+-- Point 2
+SELECT *
+FROM actors
+WHERE first_name LIKE '%s';
+
+-- Point 3
+SELECT *
+FROM movies
+WHERE rating >= 4 AND rating <= 8 AND extract(year FROM release_date) >= 2004 AND extract(year FROM release_date) >= 2010;
